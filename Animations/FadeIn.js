53e83451af5fa3283e4916d7590725cdf1d62e15
Stephane Van Pe <stephanevanpe@gmail.com>
2019-11-28 15:48:10 +0100

    Initial commit

---
+++ Animations/FadeIn.js
@@ -0,0 +1,27 @@
+// Animations/FadeIn.js
+
+import React from 'react';
+import {Animated, Dimensions} from 'react-native';
+
+class FadeIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positionLeft: new Animated.Value(Dimensions.get('window').width),
+    };
+  }
+
+  componentDidMount() {
+    Animated.spring(this.state.positionLeft, {
+      toValue: 0,
+    }).start();
+  }
+
+  render() {
+    return (
+      <Animated.View style={{left: this.state.positionLeft}}></Animated.View>
+    );
+  }
+}
+
+export default FadeIn;
